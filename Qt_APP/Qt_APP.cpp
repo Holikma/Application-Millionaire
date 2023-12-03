6ba502e23cc85401ae6649dbf8117155c80f3a00
holikma <marekholik01@gmail.com>
2023-12-03 15:11:57 +0100

    update connect

--- Qt_APP/Qt_APP.cpp
+++ Qt_APP/Qt_APP.cpp
@@ -7,6 +7,7 @@ using namespace std;
 Qt_APP::Qt_APP(QWidget *parent) : QMainWindow(parent){
     ui.setupUi(this);
     connect(ui.pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
+    connect(ui.pushButton_2, SIGNAL(clicked()), this, SLOT(on_pushButton_2_clicked()));
 }
 
 Qt_APP::~Qt_APP(){
